60168f88ef2dacd1fe7a68187b57c38923542b8b
Viola Yosevi <viola.yosevi19@gmail.com>
2024-11-25 16:27:01 +0700

    first commit

---
+++ src/med1_endoskopi/Med1_Endoskopi.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package med1_endoskopi;
+
+import med1_endoskopi.view.Login;
+
+
+
+/**
+ *
+ * @author user
+ */
+public class Med1_Endoskopi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login loginFrame = new Login();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+    }
+    
+}
